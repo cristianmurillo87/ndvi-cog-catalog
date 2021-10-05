710010d65ef3412f9d19874c8f0ec557ec587407
Cristian Andres <crisanmur@gmail.com>
2021-10-05 20:19:16 +0200

    refactor: cast band data as int

--- cog_catalog/calculator.py
+++ cog_catalog/calculator.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
-from image import COGImage, BandNotFoundException
+import numpy as np
+from cog_catalog.image import COGImage, BandNotFoundException
+from cog_catalog.band import COGImageBand
+
+np.seterr(invalid='ignore')
 
 class NDVICalculator(object):
 
@@ -16,9 +20,9 @@ class NDVICalculator(object):
         self.nir_band = nir_band
         self.red_band_name = red_band_name
         self.nir_band_name = nir_band_name
-        self.ndvi = None
+        self.__calculate_ndvi()
 
-    def calculate_ndv(self):
+    def __calculate_ndvi(self):
         if self.image:
             self.ndvi = NDVICalculator.ndvi_from_image(
                 image=self.image,
@@ -42,13 +46,15 @@ class NDVICalculator(object):
 
     @staticmethod
     def ndvi_from_image_bands(red_band, nir_band):
-        ndvi_arr = (nir_band.data - red_band.data) / (nir_band.data + red_band.data)
+        band_sub = nir_band.data.astype(int) - red_band.data.astype(int)
+        band_sum = nir_band.data.astype(int) + red_band.data.astype(int)
+        ndvi_arr = band_sub / band_sum
         ndvi = COGImageBand(
             title='NDVI',
             name='ndvi',
             band_data=ndvi_arr,
             crs=nir_band.crs,
-            bounds=nir.bounds
+            bounds=nir_band.bounds
         )
         return ndvi
 
@@ -59,19 +65,18 @@ class NDVICalculator(object):
             plt.title('NDVI (mean {})'.format(self.ndvi.mean))
             plt.xlabel('Row')
             plt.ylabel('Column')
+        else:
+            print('Statistics Summary:\nNo NDVI has yet been calculated')
 
     def summary(self):
         if self.ndvi:
-            print("""
-                Statistics summary:
-                Mean: {}\tMedian: {}
-                Std. Dev: {}\tVariance:{}
-            """.format(
+            print('Statistics summary:\nMin: {:.4f}\tMax: {:.4f}\n Mean: {:.4f}\tMedian: {:.4f}\nStd. Dev: {:.4f}\tVariance:{:.4f}'.format(
+                self.ndvi.min,
+                self.ndvi.max,
                 self.ndvi.mean,
                 self.ndvi.median,
                 self.ndvi.std,
-                self.ndiv.variance
-                )
-                )
+                self.ndvi.variance
+                ))
         else:
-            print('Statistics Summary:\nNo ndvi has yet been calculated')
+            print('Statistics Summary:\nNo NDVI has yet been calculated')
