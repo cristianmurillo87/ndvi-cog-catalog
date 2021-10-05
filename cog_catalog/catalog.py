1e6e31fb65a0eddbf688e916869d3a1a8faf7f9e
Cristian Andres <crisanmur@gmail.com>
2021-10-05 20:17:51 +0200

    fix: retrieve geometry from geojson object

--- cog_catalog/catalog.py
+++ cog_catalog/catalog.py
@@ -1,5 +1,7 @@
 import requests
-from image import COGImage
+import json
+from cog_catalog.image import COGImage
+
 
 class COGCatalog(object):
 
@@ -9,7 +11,7 @@ class COGCatalog(object):
     def __init__(
         self,
         limit=10,
-        collections=None,
+        collections=['sentinel-s2-l2a-cogs'],
         time_filter=None,
         bbox=None,
         aoi=None,
@@ -23,11 +25,13 @@ class COGCatalog(object):
     def initialize(self):
         payload = self.__generate_request_payload()
         try:
-            response = requests.post(self.__CATALOG_URL, data=payload)
-            json_response = response.json()
-            if json_response:
-                self.__update_image_list(json_response)
+            response = requests.post(self.__CATALOG_URL, json=payload)
+            if response.ok:
+                json_response = response.json()
+                if json_response:
+                    self.__update_image_list(json_response)
         except requests.HTTPError:
+            self.__IMAGE_LIST = []
             return
 
     def __generate_request_payload(self):
@@ -35,23 +39,33 @@ class COGCatalog(object):
             'limit': self.__limit
         }
 
-        if len(self.__collections) > 0:
+        if isinstance(self.__collections, list) and len(self.__collections) > 0:
             payload['collections'] = self.__collections
         
-        if len(self.__intersects) > 0:
+        if isinstance(self.__bbox, list) and len(self.__bbox) > 0:
             payload['bbox'] = self.__bbox
         
         if self.__datetime:
             payload['datetime'] = self.__datetime
         
         if self.__intersects:
-            payload['intersects'] = self.__intersects
+            try:
+                with open(self.__intersects, 'r') as geojson_file:
+                    geojson = json.load(geojson_file)
+                    features = geojson.get('features', [])
+                    if len(features) > 0:
+                        feature = features[0]
+                        payload['intersects'] = feature.get('geometry')
+            except Exception:
+                print('Error loading {}'.format(self.__intersects))
         
         if isinstance(self.__query, dict):
             payload['query'] = self.__query
 
         if len(self.__sort) > 0:
             payload['sort'] = self.__sort
+
+        return payload
 
     def __update_image_list(self, geojson):
         self.__IMAGE_LIST = []
@@ -77,7 +91,7 @@ class COGCatalog(object):
         self.__bbox = bbox
         self.__datetime = time_filter
         self.__intersects = aoi
-        self.__collections = collections or ['sentinel-s2-l2a-cogs']
+        self.__collections = collections
         self.__query = query_obj
         self.__sort = sort_list or []
         self.initialize()
@@ -86,20 +100,20 @@ class COGCatalog(object):
         if len(self.__IMAGE_LIST) >= index + 1:
             return self.__IMAGE_LIST[index]
         return None
-        
+
     def get_image_by_id(self, image_id):
-        matches = filter(lambda img: img.id == image_id, self.__IMAGE_LIST)
+        matches = list(filter(lambda img: img.id == image_id, self.__IMAGE_LIST))
         return matches[0] if len(matches) > 0 else None
 
     def get_image_by_product_id(self, product_id):
-        matches = filter(lambda img: img.product_id == product_id, self.__IMAGE_LIST)
+        matches = list(filter(lambda img: img.product_id == product_id, self.__IMAGE_LIST))
         return matches[0] if len(matches) > 0 else None
 
     def get_images_by_collection(self, collection_name):
-        return filter(lambda img: img.collection == collection_name, self.__IMAGE_LIST)
+        return list(filter(lambda img: img.collection == collection_name, self.__IMAGE_LIST))
 
     def get_images_by_platform(self, platform):
-        return filter(lambda img: img.platform == platform, self.__IMAGE_LIST)
+        return list(filter(lambda img: img.platform == platform, self.__IMAGE_LIST))
     
-    def get_images_by_platform(self, constellation):
-        return filter(lambda img: img.constellation == constellation, self.__IMAGE_LIST)
+    def get_images_by_constellation(self, constellation):
+        return list(filter(lambda img: img.constellation == constellation, self.__IMAGE_LIST))
